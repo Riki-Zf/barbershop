c8c1538709a6ce95f27f592285310a3b1f8449c5
Riki-Zf <rikipatande81979@gmail.com>
2025-02-07 22:47:09 +0800

    make slider logic

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -1,21 +1,72 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Service.css";
 import srvImg from "../../assets/img1.jpg";
 const Service = ({ children }) => {
+  const slider = useRef();
+  let tx = 0;
+
+  const slideForward = () => {
+    if (tx > -70) {
+      tx -= 50;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
+
+  const slideBackward = () => {
+    if (tx < 0) {
+      tx += 50;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
+
   return (
     <div className="service">
       <div className="service-left">{children}</div>
       <div className="service-right">
-        <div className="card-slider">
-          <img src={srvImg} alt="" />
+        <i className="next-btn fa-solid fa-solid fa-angle-right" onClick={slideForward}></i>
+        <i className="back-btn fa-solid fa-solid fa-angle-left" onClick={slideBackward}></i>
+        <div className="slider">
+          <ul ref={slider}>
+            <li>
+              <div className="card-slider">
+                <img src={srvImg} alt="" />
 
-          <div className="srv-info">
-            <h3>judul</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus obcaecati asperiores aliquid tenetur, voluptatum expedita, necessitatibus placeat aperiam architecto nemo iusto iure fugiat explicabo laborum dolor error dicta
-              accusamus sunt.
-            </p>
-          </div>
+                <div className="srv-info">
+                  <h3>judul</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus obcaecati asperiores aliquid tenetur, voluptatum expedita, necessitatibus placeat aperiam architecto nemo iusto iure fugiat explicabo laborum dolor error
+                    dicta accusamus sunt.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="card-slider">
+                <img src={srvImg} alt="" />
+
+                <div className="srv-info">
+                  <h3>judul 2</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus obcaecati asperiores aliquid tenetur, voluptatum expedita, necessitatibus placeat aperiam architecto nemo iusto iure fugiat explicabo laborum dolor error
+                    dicta accusamus sunt.
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* <li>
+              <div className="card-slider">
+                <img src={srvImg} alt="" />
+
+                <div className="srv-info">
+                  <h3>judul </h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus obcaecati asperiores aliquid tenetur, voluptatum expedita, necessitatibus placeat aperiam architecto nemo iusto iure fugiat explicabo laborum dolor error
+                    dicta accusamus sunt.
+                  </p>
+                </div>
+              </div>
+            </li> */}
+          </ul>
         </div>
       </div>
     </div>
