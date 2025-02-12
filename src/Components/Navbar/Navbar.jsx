94ef1d1d97ad1d8071ecb505c89cb8aab0237884
Riki-Zf <rikipatande81979@gmail.com>
2025-02-12 22:30:06 +0800

    add navbar mobile view

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css";
 import logo from "../../assets/Logo.png";
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 50) {
@@ -20,14 +20,34 @@ const Navbar = () => {
     };
   }, []);
 
+  //toggle menu
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
     <nav className={`container ${isScrolled ? "scrolled" : ""}`}>
-      <ul>
+      <img src={logo} alt="" className="mobile-logo" />
+
+      <i className="menu-bar fa-solid fa-bars" onClick={toggleSidebar}></i>
+      <ul className={`sidebar ${isSidebarOpen ? "active" : ""}`}>
+        <i className="fa-solid fa-times" style={{ color: "#76abae", alignSelf: "flex-end", cursor: "pointer", fontSize: "20px" }} onClick={closeSidebar} />
+        <img src={logo} alt="" className="logo" />
         <li>Pricelist</li>
         <li>Lokasi</li>
+        <li>Trends</li>
+        <li>Academy</li>
+      </ul>
+      <ul>
         <img src={logo} alt="" className="logo" />
-        <li>Hair Trends</li>
-        <li>Captain Academy</li>
+        <li>Pricelist</li>
+        <li>Lokasi</li>
+        <li>Trends</li>
+        <li>Academy</li>
       </ul>
     </nav>
   );
