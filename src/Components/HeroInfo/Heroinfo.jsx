288632eb42cc239685bd824495de5f30a0114f90
Riki-Zf <rikipatande81979@gmail.com>
2025-02-13 06:41:02 +0800

    add mobile view for HeroInfo section

--- src/Components/HeroInfo/Heroinfo.jsx
+++ src/Components/HeroInfo/Heroinfo.jsx
@@ -1,35 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Heroinfo.css";
 const Heroinfo = () => {
-  return (
-    <div className="hero">
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const heroData = [
+    {
+      title: "Layanan Pelanggan",
+      icon: "fa-brands fa-whatsapp",
+      content: (
+        <>
+          08123334444 <br />
+          Kapten
+        </>
+      ),
+    },
+    {
+      title: "kantor pusat",
+      icon: "fa-solid fa-location-dot",
+      content: "Lorem ipsum dolor sit amet consectetur.",
+    },
+    {
+      title: "Jam Operasional",
+      icon: "fa-regular fa-clock",
+      content: "10.00-22.00 Lorem ipsum dolor sit amet.",
+    },
+  ];
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev === heroData.length - 1 ? 0 : prev + 1));
+  };
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev === 0 ? heroData.length - 1 : prev - 1));
+  };
+
+  //desktop
+  const desktopView = (
+    <div className="hero desktop-view">
+      {heroData.map((item, index) => (
+        <div key={index} className="hero-info">
+          <h3>{item.title}</h3>
+          <div className="info-content">
+            <i className={item.icon}></i>
+            <p>{item.content}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+
+  const mobileView = (
+    <div className="hero mobile-view">
+      <button className="slider-button prev" onClick={prevSlide}>
+        <i className="fa-solid fa-chevron-left"></i>
+      </button>
+
       <div className="hero-info">
-        <h3>Layanan Pelanggan</h3>
+        <h3>{heroData[currentSlide].title}</h3>
         <div className="info-content">
-          <i className="fa-brands fa-whatsapp"></i>
-          <p>
-            081234567899 <br />
-            Kapten
-          </p>
+          <i className={heroData[currentSlide].icon}></i>
+          <p>{heroData[currentSlide].content}</p>
         </div>
       </div>
 
-      <div className="hero-info">
-        <h3>Kantor Pusat</h3>
-        <div className="info-content">
-          <i className="fa-solid fa-location-dot"></i>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-        </div>
-      </div>
+      <button className="slider-button next" onClick={nextSlide}>
+        <i className="fa-solid fa-chevron-right"></i>
+      </button>
 
-      <div className="hero-info">
-        <h3>Jam Operasional</h3>
-        <div className="info-content">
-          <i className="fa-regular fa-clock"></i>
-          <p>10.00 - 22.00 Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-        </div>
+      <div className="slider-dots">
+        {heroData.map((_, index) => (
+          <span
+            key={index}
+            className={`dot ${currentSlide === index ? "active" : ""}`}
+            onClick={() => {
+              setCurrentSlide(index);
+            }}
+          />
+        ))}
       </div>
     </div>
+  );
+
+  return (
+    <>
+      {desktopView}
+      {mobileView}
+    </>
   );
 };
 
